39cb10fdab98ec0a31d7e050c510f6ccca9ab409
solicht88 <sakanaasai@gmail.com>
2024-06-08 11:15:55 -0400

    placeholder flask app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('index.html', message='Hello, World!')
 
-# use 'flask app run' then go to http://127.0.0.1:5000/ to preview
+@app.route("/test")
+def placeholder():
+    return render_template('index.html', message='I am 7 years old')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# use 'python app.py' then go to http://127.0.0.1:5000/ to preview
