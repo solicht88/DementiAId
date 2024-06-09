4f93158b97158da6a730549ad56f50e524359e93
solicht88 <sakanaasai@gmail.com>
2024-06-09 00:09:32 -0400

    Small fixes, added blog page

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def reminder():
     return render_template('reminders.html')
 
+@app.route("/blog/")
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
